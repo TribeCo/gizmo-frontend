2a447aceb457513b8b060e6e0a03d25394eb8dfd
reza buzarjemehri <dev.reza21@gmail.com>
2024-02-15 23:15:07 +0330

    create blog card

--- src/components/CardProductSlider.jsx
+++ src/components/CardProductSlider.jsx
@@ -1,16 +1,11 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
-import { 
-  Paper, 
-  Typography, 
-  Grid, 
-  IconButton 
-} from '@mui/material';
-import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
-import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
+import React, { useState, useEffect } from "react";
+import { Box, Button, Grid, IconButton, Typography } from "@mui/material";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
 
-import { Colors } from '@/utils';
+import { Colors } from "@/utils";
 import { ProductCard } from ".";
 
 {
@@ -43,7 +38,7 @@ const CardProductSlider = ({
 		}, swapTime_millisecond);
 		return () => clearInterval(interval);
 	}, [currentIndex]);
-  
+
 	const handleNext = () => {
 		setCurrentIndex((prevIndex) =>
 			prevIndex === products.length - 1 ? 0 : prevIndex + 1,
@@ -62,53 +57,86 @@ const CardProductSlider = ({
 	}
 
 	return (
-		<Grid 
-    pl={3} pr={3}
-      container 
-      justifyContent='space-between' 
-      alignItems='center' 
-    >
-      <Grid item>
-        <IconButton
-          variant='contained'
-          sx={{
-            bgcolor: Colors.blue,
-            position: 'relative',
-            '&:hover': {
-              backgroundColor: Colors.blue,
-            },
-          }}
-          onClick={handleNext}
-        >
-          <ArrowForwardIosOutlinedIcon sx={{ color: "white" }} />
-        </IconButton>
-      </Grid>
+		<Grid md={12}>
 			<Grid
-				item
-				display="flex">
-				{productItems.map((product, index) => (
-					<ProductCard
-						key={index}
-						product={product}
-					/>
-				))}
+				pl={3}
+				pr={3}
+				container
+				position="absolute"
+				justifyContent="space-between"
+				alignItems="center">
+				<Grid item>
+					<IconButton
+						variant="contained"
+						sx={{
+							bgcolor: Colors.blue,
+							position: "relative",
+							"&:hover": {
+								backgroundColor: Colors.blue,
+							},
+						}}
+						onClick={handleNext}>
+						<ArrowForwardIosOutlinedIcon sx={{ color: "white" }} />
+					</IconButton>
+				</Grid>
+				<Grid
+					item
+					display="flex">
+					{productItems.map((product, index) => (
+						<ProductCard
+							key={index}
+							product={product}
+						/>
+					))}
+				</Grid>
+				<Grid item>
+					<IconButton
+						variant="contained"
+						sx={{
+							bgcolor: Colors.blue,
+							position: "relative",
+							"&:hover": {
+								backgroundColor: Colors.blue,
+							},
+						}}
+						onClick={handlePrev}>
+						<ArrowBackIosOutlinedIcon sx={{ color: "white" }} />
+					</IconButton>
+				</Grid>
 			</Grid>
-      <Grid item>
-        <IconButton
-          variant='contained'
-          sx={{
-            bgcolor: Colors.blue,
-            position: 'relative',
-            '&:hover': {
-              backgroundColor: Colors.blue,
-            },
-          }}
-          onClick={handlePrev}
-        >
-          <ArrowBackIosOutlinedIcon sx={{ color: "white" }} />
-        </IconButton>
-      </Grid>
-    </Grid>
-  );
-
+			<Box>
+				<Box height="240px"></Box>
+				<Box
+					height="190px"
+					bgcolor="#DEF0F5"></Box>
+			</Box>
+			<Box mt={10}>
+				<Button
+					variant="contained"
+					sx={{
+						display: "block",
+						mx: "auto",
+						border: `3px solid ${Colors.orange}`,
+						color: "#000000",
+						bgcolor: "#FFFFFF",
+						borderRadius: "30px",
+						width: "300px",
+						height: "60px",
+						"&:hover": {
+							bgcolor: Colors.orange,
+						},
+					}}>
+					<Typography
+						variant="h5"
+						sx={{
+							fontWeight: "700",
+							fontSize: "20px",
+						}}>
+						{"مشاهده محصولات بیشتر"}
+					</Typography>
+				</Button>
+			</Box>
+		</Grid>
+	);
+};
 export default CardProductSlider;
--- src/components/LineSpliter.jsx
+++ src/components/LineSpliter.jsx
@@ -1,47 +1,40 @@
-import React from 'react';
-import { 
-    Box, 
-    Typography 
-} from '@mui/material';
-import { Colors } from '@/utils';
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { Colors } from "@/utils";
 
 const LineSplitter = ({ text }) => {
-    return (
-        <Box 
-            mt={10} 
-            mb={10} 
-            display="flex" 
-            alignItems="center" 
-            justifyContent="center"
-        >
-            <Box 
-                ml={6} 
-                width="100%" 
-                borderBottom={5} 
-                borderColor={Colors.blue} 
-                flexWrap="nowrap"
-            />
-            <Typography
-                dir="center"
-                fontWeight="bold"
-                variant="h5"
-                sx={{ 
-                    px: 4, 
-                    color: 'black', 
-                    bgcolor: 'white',
-                    whiteSpace: 'nowrap'
-                }}
-            >
-                {text}
-            </Typography>
-            <Box 
-                mr={6} 
-                width="100%" 
-                borderBottom={5} 
-                borderColor={Colors.blue} 
-            />
-        </Box>
-    );
-}
+	return (
+		<Box
+			mt={10}
+			mb={10}
+			display="flex"
+			alignItems="center"
+			justifyContent="center">
+			<Box
+				ml={6}
+				width="100%"
+				borderBottom={5}
+				borderColor={Colors.blue}
+				flexWrap="nowrap"
+			/>
+			<Typography
+				dir="center"
+				fontWeight="900"
+				variant="h4"
+				sx={{
+					px: 3,
+					whiteSpace: "nowrap",
+				}}>
+				{text}
+			</Typography>
+			<Box
+				mr={6}
+				width="100%"
+				borderBottom={5}
+				borderColor={Colors.blue}
+			/>
+		</Box>
+	);
+};
 
 export default LineSplitter;

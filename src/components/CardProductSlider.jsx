9ced0b08fc82d2761040fad6c021059bbe8631c2
reza buzarjemehri <dev.reza21@gmail.com>
2024-02-15 20:59:42 +0330

    create product card

--- src/components/CardProductSlider.jsx
+++ src/components/CardProductSlider.jsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
-import { Paper, Typography, Grid, IconButton } from '@mui/material';
-import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
-import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
+import React, { useState, useEffect } from "react";
+import { Paper, Typography, Grid, IconButton } from "@mui/material";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
+import { ProductCard } from ".";
 
-
-{/*
+{
+	/*
     Inputs:
     products: a list of all product cards in folowing format:
 		[
@@ -19,85 +20,87 @@ import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
         itemsPerPage: number of product cards to show in page 
     
     swapTime_millisecond: delay time for automatic slider 
-*/}
+*/
+}
 
-const CardProductSlider = ({ products, itemsPerPage, swapTime_millisecond }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+const CardProductSlider = ({
+	products,
+	itemsPerPage,
+	swapTime_millisecond,
+}) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      handleNext();
-    }, swapTime_millisecond);
-    return () => clearInterval(interval);
-  }, [currentIndex]);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			handleNext();
+		}, swapTime_millisecond);
+		return () => clearInterval(interval);
+	}, [currentIndex]);
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === products.length - 1 ? 0 : prevIndex + 1));
-  };
+	const handleNext = () => {
+		setCurrentIndex((prevIndex) =>
+			prevIndex === products.length - 1 ? 0 : prevIndex + 1,
+		);
+	};
 
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? products.length - 1 : prevIndex - 1));
-  };
+	const handlePrev = () => {
+		setCurrentIndex((prevIndex) =>
+			prevIndex === 0 ? products.length - 1 : prevIndex - 1,
+		);
+	};
 
-  const productItems = [];
-  for (let index = 0; index < itemsPerPage; index++) {
-    productItems.push(products[(currentIndex + index) % products.length]);
-  }
+	const productItems = [];
+	for (let index = 0; index < itemsPerPage; index++) {
+		productItems.push(products[(currentIndex + index) % products.length]);
+	}
 
-  return (
-    <Grid 
-    pl={3} pr={3}
-      container 
-      justifyContent='space-between' 
-      alignItems='center' 
-    >
-      <Grid item>
-        <IconButton
-          variant='contained'
-          sx={{
-            bgcolor: '#22668D',
-            position: 'relative',
-            '&:hover': {
-              backgroundColor: "#22668D",
-            },
-          }}
-          onClick={handleNext}
-        >
-          <ArrowForwardIosOutlinedIcon sx={{ color: "white" }} />
-        </IconButton>
-      </Grid>
-
-
-      {productItems.map((product, index) => (
-        <Grid key={index} xs={1}>
-          <Paper elevation={10} style={{ padding: '20px', height: '100%' }}>
-            <Typography variant="h5" component="div">
-              {product.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {product.description}
-            </Typography>
-          </Paper>
-        </Grid>
-      ))}
-
-      <Grid item>
-        <IconButton
-          variant='contained'
-          sx={{
-            bgcolor: '#22668D',
-            position: 'relative',
-            '&:hover': {
-              backgroundColor: "#22668D",
-            },
-          }}
-          onClick={handlePrev}
-        >
-          <ArrowBackIosOutlinedIcon sx={{ color: "white" }} />
-        </IconButton>
-      </Grid>
-    </Grid>
-  );
+	return (
+		<Grid
+			pl={5}
+			pr={5}
+			container
+			justifyContent="space-between"
+			alignItems="center">
+			<Grid item>
+				<IconButton
+					variant="contained"
+					sx={{
+						bgcolor: "#22668D",
+						position: "relative",
+						"&:hover": {
+							backgroundColor: "#22668D",
+						},
+					}}
+					onClick={handleNext}>
+					<ArrowForwardIosOutlinedIcon sx={{ color: "white" }} />
+				</IconButton>
+			</Grid>
+			<Grid
+				item
+				display="flex">
+				{productItems.map((product, index) => (
+					<ProductCard
+						key={index}
+						product={product}
+					/>
+				))}
+			</Grid>
+			<Grid item>
+				<IconButton
+					variant="contained"
+					sx={{
+						bgcolor: "#22668D",
+						position: "relative",
+						"&:hover": {
+							backgroundColor: "#22668D",
+						},
+					}}
+					onClick={handlePrev}>
+					<ArrowBackIosOutlinedIcon sx={{ color: "white" }} />
+				</IconButton>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default CardProductSlider;

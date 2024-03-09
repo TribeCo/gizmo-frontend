01c521f555d39ca425322916bf45dea992426c6e
Parsa Vazifeh <p_vazifeh@comp.iust.ac.ir>
2024-03-09 18:19:11 +0330

    nearly finished

--- src/components/dashboard/DashBoardOrders.jsx
+++ src/components/dashboard/DashBoardOrders.jsx
@@ -1,8 +1,10 @@
 'use client'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
+import { Box } from '@mui/material'
 
 import searchIcon from '@/components/siteIcons/SearchIcon.svg'
+import LevelofOrdering from './LevelofOrdering'
 
 export default function DashBoardOrders() {
 
@@ -14,19 +16,19 @@ export default function DashBoardOrders() {
     }
 
     const [realOrders, setRealOrders] = useState([
-        {orderCode: '0786453465' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
-        {orderCode: '4894545456' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
-        {orderCode: '4128546528' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
-        {orderCode: '4654894898' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
-        {orderCode: '0782586558' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
+        { orderCode: '0786453465', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
+        { orderCode: '4894545456', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
+        { orderCode: '4128546528', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
+        { orderCode: '4654894898', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
+        { orderCode: '0782586558', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
     ])
 
     const [orders, setOrders] = useState([
-        {orderCode: '0786453465' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
-        {orderCode: '4894545456' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
-        {orderCode: '4128546528' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
-        {orderCode: '4654894898' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
-        {orderCode: '0782586558' , destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date:'1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000'},
+        { orderCode: '0786453465', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
+        { orderCode: '4894545456', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
+        { orderCode: '4128546528', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
+        { orderCode: '4654894898', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
+        { orderCode: '0782586558', destinationAddress: 'شیراز، دانشگاه صنعتی شیراز', receiverName: 'سامان', date: '1402/5/6', discountPrice: '0', price: '398000', finalPrice: '398000' },
     ])
 
     const searchProductCode = (event) => {
@@ -39,7 +41,7 @@ export default function DashBoardOrders() {
 
     const retrieveData = (event) => {
         let key = event.keyCode || event.charCode;
-        if( key == 8 || key == 46 )event.target.value = ''
+        if (key == 8 || key == 46) event.target.value = ''
         setOrders(realOrders)
     }
 
@@ -50,107 +52,103 @@ export default function DashBoardOrders() {
         event.target.nextSibling.classList.toggle('rotate-180')
     }
 
-  return (
-    <div>
-    
+    return (
+        <div>
 
-          <section className=" py-4 px-[4%] w-[60rem] flex flex-col h-[47rem] overflow-scroll lg:w-full rounded-xl shadow-lg">
 
-            <div className="flex border-b border-[#EDEDED] justify-between py-2 gap-2 mb-4">
-                <h3 className='font-bold flex items-center text-lg md:text-md'>
-                    سفارشات
-                </h3>
+            <section className=" py-4 px-[4%] w-[60rem] flex flex-col h-[47rem] overflow-scroll lg:w-full rounded-xl shadow-lg">
 
-                <div className='flex border rounded-2xl overflow-hidden'>
+                <div className="flex border-b border-[#EDEDED] justify-between py-2 gap-2 mb-4">
+                    <h3 className='font-bold flex items-center text-lg md:text-md'>
+                        سفارشات
+                    </h3>
 
-                    <input onInput={handleInputChange} onKeyDown={retrieveData} className='w-[85%] p-2' type="text" placeholder='جستوجو کد پیگیری'/>
+                    <div className='flex border rounded-2xl overflow-hidden'>
 
-                    <div onClick={searchProductCode} className='bg-palette-blue w-[15%] flex items-center justify-center cursor-pointer'>
-                        <Image src={searchIcon} width={16}></Image>
+                        <input onInput={handleInputChange} onKeyDown={retrieveData} className='w-[85%] p-2' type="text" placeholder='جستوجو کد پیگیری' />
+
+                        <div onClick={searchProductCode} className='bg-palette-blue w-[15%] flex items-center justify-center cursor-pointer'>
+                            <Image src={searchIcon} width={16}></Image>
+                        </div>
+
                     </div>
-                  
                 </div>
-            </div>
 
-            <section className='flex flex-col mt-4 px-4 gap-2 overflow-scroll'>
+                <section className='flex flex-col mt-4 px-4 gap-2 overflow-scroll'>
 
-              {orders.map((order) => (
-                <div className="cursor-pointer hover:bg-[#00000011] transition-all flex flex-col gap-6 bg-[#F7F7F7] rounded-lg md:flex-col md:gap-4">
+                    {orders.map((order) => (
+                        <div className="cursor-pointer hover:bg-[#00000011] transition-all flex flex-col gap-6 bg-[#F7F7F7] rounded-lg md:flex-col md:gap-4">
 
-                    <div className="OrderCard flex gap-6 px-6 py-6 md:flex-col md:gap-4">
+                            <div className="OrderCard flex gap-6 px-6 py-6 md:flex-col md:gap-4">
 
-                    <div className="rightPart flex w-full flex-col items-end gap-4">
+                                <div className="rightPart flex w-full flex-col items-end gap-4">
 
-                        <div className="infoRow flex w-full gap-6 md:gap-0 md:justify-between">
-                            <span  className='font-extrabold md:text-sm'>کد پیگیری: </span>
-                            <div  className=' md:text-sm'>{order.orderCode}</div>
-                        </div>
+                                    <div className="infoRow flex w-full gap-6 md:gap-0 md:justify-between">
+                                        <span className='font-extrabold md:text-sm'>کد پیگیری: </span>
+                                        <div className=' md:text-sm'>{order.orderCode}</div>
+                                    </div>
 
-                        <div className="infoRow flex w-full gap-6 md:gap-4 md:justify-between">
-                            <span  className='font-extrabold md:text-sm whitespace-nowrap'>آدرس ارسال: </span>
-                            <div  className=' md:text-xs'>{order.destinationAddress}</div>
-                        </div>
+                                    <div className="infoRow flex w-full gap-6 md:gap-4 md:justify-between">
+                                        <span className='font-extrabold md:text-sm whitespace-nowrap'>آدرس ارسال: </span>
+                                        <div className=' md:text-xs'>{order.destinationAddress}</div>
+                                    </div>
 
-                        <div className="infoRow flex w-full gap-6 md:gap-0 md:justify-between">
-                            <span  className='font-extrabold md:text-sm'>نام گیرنده: </span>
-                            <div  className=' md:text-sm'>{order.receiverName}</div>
-                        </div>
+                                    <div className="infoRow flex w-full gap-6 md:gap-0 md:justify-between">
+                                        <span className='font-extrabold md:text-sm'>نام گیرنده: </span>
+                                        <div className=' md:text-sm'>{order.receiverName}</div>
+                                    </div>
 
-                        <div className="infoRow flex w-full gap-6 md:gap-0 md:justify-between">
-                            <span  className='font-extrabold md:text-sm'>تاریخ سفارش: </span>
-                            <div  className=' md:text-sm'>{order.date}</div>
-                        </div>
+                                    <div className="infoRow flex w-full gap-6 md:gap-0 md:justify-between">
+                                        <span className='font-extrabold md:text-sm'>تاریخ سفارش: </span>
+                                        <div className=' md:text-sm'>{order.date}</div>
+                                    </div>
 
-                    </div>
+                                </div>
 
-                    <div className="leftPart w-full flex flex-col gap-4">
+                                <div className="leftPart w-full flex flex-col gap-4">
 
-                        <div className="infoRow flex w-full justify-between">
-                            <span  className='font-extrabold md:text-xs'>قیمت کل سفارش: </span>
-                            <div  className='flex gap-1'>
-                                <span className=' md:text-sm'>{order.price}</span>
-                                <span className=' md:text-sm'>تومان</span>
+                                    <div className="infoRow flex w-full justify-between">
+                                        <span className='font-extrabold md:text-xs'>قیمت کل سفارش: </span>
+                                        <div className='flex gap-1'>
+                                            <span className=' md:text-sm'>{order.price}</span>
+                                            <span className=' md:text-sm'>تومان</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="infoRow flex w-full justify-between">
+                                        <span className='font-extrabold md:text-sm'>میزان تخفیف: </span>
+                                        <div className='flex gap-1'>
+                                            <span className=' md:text-sm'>{order.discountPrice}</span>
+                                            <span className=' md:text-sm'>تومان</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="infoRow flex w-full justify-between">
+                                        <span className='font-extrabold md:text-xs'>مبلغ قابل پرداخت: </span>
+                                        <div className='flex gap-1 font-extrabold'>
+                                            <span className=' md:text-sm'>{order.finalPrice}</span>
+                                            <span className=' md:text-sm'>تومان</span>
+                                        </div>
+                                    </div>
+
+                                    <div className="infoRow flex w-full gap-2 justify-end items-center select-none">
+                                        <span onClick={drawerHandler} className='font-extrabold text-palette-blue md:text-sm'>پیگیری سفارش</span>
+                                        <svg className='transition-all' width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M6.70337 9.51299L0.330366 3.13998C-0.110122 2.6995 -0.110122 1.98722 0.330366 1.55142L1.38941 0.492373C1.8299 0.0518866 2.54217 0.0518866 2.97798 0.492373L7.5 5.00503L12.0173 0.487687C12.4578 0.0472002 13.1701 0.0472002 13.6059 0.487687L14.6696 1.54673C15.1101 1.98722 15.1101 2.6995 14.6696 3.1353L8.29663 9.50831C7.85614 9.95348 7.14386 9.95348 6.70337 9.51299Z" fill="#22668D" />
+                                        </svg>
+                                    </div>
+
+                                </div>
+
                             </div>
+                            <LevelofOrdering level={2}/>
                         </div>
+                    ))}
 
-                        <div className="infoRow flex w-full justify-between">
-                            <span  className='font-extrabold md:text-sm'>میزان تخفیف: </span>
-                            <div  className='flex gap-1'>
-                                <span className=' md:text-sm'>{order.discountPrice}</span>
-                                <span className=' md:text-sm'>تومان</span>
-                            </div>
-                        </div>
-
-                        <div className="infoRow flex w-full justify-between">
-                            <span  className='font-extrabold md:text-xs'>مبلغ قابل پرداخت: </span>
-                            <div  className='flex gap-1 font-extrabold'>
-                                <span className=' md:text-sm'>{order.finalPrice}</span>
-                                <span className=' md:text-sm'>تومان</span>
-                            </div>
-                        </div>
-
-                        <div className="infoRow flex w-full gap-2 justify-end items-center select-none">
-                            <span onClick={drawerHandler} className='font-extrabold text-palette-blue md:text-sm'>پیگیری سفارش</span>
-                            <svg className='transition-all' width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M6.70337 9.51299L0.330366 3.13998C-0.110122 2.6995 -0.110122 1.98722 0.330366 1.55142L1.38941 0.492373C1.8299 0.0518866 2.54217 0.0518866 2.97798 0.492373L7.5 5.00503L12.0173 0.487687C12.4578 0.0472002 13.1701 0.0472002 13.6059 0.487687L14.6696 1.54673C15.1101 1.98722 15.1101 2.6995 14.6696 3.1353L8.29663 9.50831C7.85614 9.95348 7.14386 9.95348 6.70337 9.51299Z" fill="#22668D"/>
-                            </svg>
-                        </div>
-
-                    </div>
-                    
-                    </div>
-
-                    <div className="w-full hidden">
-                        Hello
-                    </div>
-
-                </div>
-              ))}
+                </section>
 
             </section>
 
-          </section>
-    
-    </div>
-  )
+        </div>
+    )
 }
---
+++ src/components/dashboard/LevelofOrdering.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const LevelofOrdering = ({ level }) => {
+    // Function to render the correct SVG based on the level
+    const renderSvgForLevel = () => {
+        switch (level) {
+            case 1:
+                return (
+                    <svg width="680" height="200" viewBox="-50 0 740 84" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M639.333 42C639.333 30.2179 629.782 20.6667 618 20.6667C606.218 20.6667 596.667 30.2179 596.667 42C596.667 53.7821 606.218 63.3333 618 63.3333C629.782 63.3333 639.333 53.7821 639.333 42ZM618 38L440 38L440 46L618 46L618 38Z" fill="#22668D" />
+                        <path d="M625 37L615 47L610 42" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M440.333 42C440.333 30.2179 430.782 20.6667 419 20.6667C407.218 20.6667 397.667 30.2179 397.667 42C397.667 53.7821 407.218 63.3333 419 63.3333C430.782 63.3333 440.333 53.7821 440.333 42ZM419 38L241 38L241 46L419 46L419 38Z" fill="#FFCC70" fill-opacity="0.42" />
+                        <path d="M241.333 42C241.333 30.2179 231.782 20.6667 220 20.6667C208.218 20.6667 198.667 30.2179 198.667 42C198.667 53.7821 208.218 63.3333 220 63.3333C231.782 63.3333 241.333 53.7821 241.333 42ZM43.3333 42C43.3333 30.2179 33.7821 20.6667 22 20.6667C10.2179 20.6667 0.666672 30.2179 0.666672 42C0.666671 53.7821 10.2179 63.3333 22 63.3333C33.7821 63.3333 43.3333 53.7821 43.3333 42ZM220 38L22 38L22 46L220 46L220 38Z" fill="#FFCC70" fill-opacity="0.42" />
+                        <rect x="213.5" y="34.5" width="15" height="15" rx="4.5" stroke="#747678" />
+                        <rect x="411.5" y="34.5" width="15" height="15" rx="4.5" stroke="#747678" />
+                        <rect x="15.5" y="34.5" width="15" height="15" rx="4.5" stroke="#747678" />
+                        <image href="https://gizmoshop.liara.run/images/media/pictures/FirstLevel.png" x="580" y="-60" width="70" height="70" />
+                        <image href="https://gizmoshop.liara.run/images/media/pictures/SecondLevel.png" x="380" y="-60" width="70" height="70" />
+                        <image href="https://gizmoshop.liara.run/images/media/pictures/ThirdLevel.png" x="180" y="-60" width="70" height="70" />
+                        <image href="https://gizmoshop.liara.run/images/media/pictures/FourthLevel.png" x="-20" y="-60" width="70" height="70" />
+                    </svg>
+                );
+            case 2:
+                return (
+                    <svg width="680" height="200" viewBox="-50 -20 740 84" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M639.333 22C639.333 10.2179 629.782 0.666667 618 0.666666C606.218 0.666665 596.667 10.2179 596.667 22C596.667 33.7821 606.218 43.3333 618 43.3333C629.782 43.3333 639.333 33.7821 639.333 22ZM618 18L440 18L440 26L618 26L618 18Z" fill="#22668D" />
+                        <path d="M625 17L615 27L610 22" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M440.333 22C440.333 10.2179 430.782 0.666667 419 0.666666C407.218 0.666665 397.667 10.2179 397.667 22C397.667 33.7821 407.218 43.3333 419 43.3333C430.782 43.3333 440.333 33.7821 440.333 22ZM419 18L241 18L241 26L419 26L419 18Z" fill="#22668D" />
+                        <path d="M427 17L417 27L412 22" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M241.333 22C241.333 10.2179 231.782 0.666667 220 0.666666C208.218 0.666665 198.667 10.2179 198.667 22C198.667 33.7821 208.218 43.3333 220 43.3333C231.782 43.3333 241.333 33.7821 241.333 22ZM43.3333 22C43.3333 10.2179 33.7821 0.66665 22 0.666649C10.2179 0.666648 0.666673 10.2179 0.666672 22C0.666671 33.7821 10.2179 43.3333 22 43.3333C33.7821 43.3333 43.3333 33.7821 43.3333 22ZM220 18L22 18L22 26L220 26L220 18Z" fill="#FFCC70" fill-opacity="0.42" />
+                        <rect x="213.5" y="14.5" width="15" height="15" rx="4.5" stroke="#747678" />
+                        <rect x="15.5" y="14.5" width="15" height="15" rx="4.5" stroke="#747678" />
+                        <image href="https://gizmoshop.liara.run/images/media/pictures/FirstLevel.png" x="580" y="-80" width="70" height="70" />
+                        <image href="https://gizmoshop.liara.run/images/media/pictures/SecondLevel.png" x="380" y="-80" width="70" height="70" />
+                        <image href="https://gizmoshop.liara.run/images/media/pictures/ThirdLevel.png" x="180" y="-80" width="70" height="70" />
+                        <image href="https://gizmoshop.liara.run/images/media/pictures/FourthLevel.png" x="-20" y="-80" width="70" height="70" />
+                    </svg>
+                );
+            case 3:
+                return (
+                    <svg>
+                        {/* SVG for level 3 */}
+                    </svg>
+                );
+            case 4:
+                return (
+                    <svg>
+                        {/* SVG for level 4 */}
+                    </svg>
+                );
+            default:
+                return null; // Or a default SVG if level is not 1, 2, 3, or 4
+        }
+    };
+
+    return (
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            {renderSvgForLevel()}
+        </Box>
+    );
+};
+
+export default LevelofOrdering;

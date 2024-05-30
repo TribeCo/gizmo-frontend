e06ae21c81ccd73b112a5fc5c730f968f2ae98f4
reza buzarjemehri <dev.reza21@gmail.com>
2024-05-30 21:07:44 +0330

    Update DashBoardFavoriteProduct.jsx

--- src/components/dashboard/DashBoardFavoriteProduct.jsx
+++ src/components/dashboard/DashBoardFavoriteProduct.jsx
@@ -1,107 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Divider, Grid, Typography, Paper } from '@mui/material';
-import ProductCard from '@/components/ProductCard';
-import { fetchFavoriteProducts } from '@/services/DashBoard';
-import { useAuth } from '@/context/AuthContext';
+import React, { useState, useEffect } from "react";
+import { Box, Divider, Grid, Typography, Paper } from "@mui/material";
+import ProductCard from "@/components/ProductCard";
+import { fetchFavoriteProducts } from "@/services/DashBoard";
+import { useAuth } from "@/context/AuthContext";
 import { enqueueSnackbar } from "notistack";
 export default function DashBoardFavoriteProduct() {
+	const [products, setProducts] = useState([]);
+	const { tokens } = useAuth();
 
-    const[products, setProducts] = useState([])
-    const { tokens } = useAuth();
-    
-    useEffect(() => {
-        if(tokens) {
-            GetFavoriteProducts();
-        }
-    }, [tokens]);
+	useEffect(() => {
+		if (tokens) {
+			GetFavoriteProducts();
+		}
+	}, [tokens]);
 
-    const GetFavoriteProducts = async () => {
-        try {
-            const response = await fetchFavoriteProducts(tokens);
-            if (response) {
-                setProducts(response.data);
-            }
-        } catch (error) {
-            console.error('Error fetching favorite products:', error);
-            enqueueSnackbar({ message: error.message || "خطا در دریافت محصولات مورد علاقه.", variant: "error" });
-        }
-    };
+	const GetFavoriteProducts = async () => {
+		try {
+			const response = await fetchFavoriteProducts(tokens);
+			if (response) {
+				setProducts(response.data);
+			}
+		} catch (error) {
+			console.error("Error fetching favorite products:", error);
+			enqueueSnackbar({
+				message: error.message || "خطا در دریافت محصولات مورد علاقه.",
+				variant: "error",
+			});
+		}
+	};
 
-    return (
-        <Paper
-            variant="outlined"
-            sx={{
-                height: 'fit-content',
-                borderRadius: '15px',
-                boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.1)',
-            }}
-        >
-            <Box
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    flexDirection: 'column',
-                    padding: { xs: '15px', md: '30px' },
-                    maxWidth: '900px',
-                    maxHeight: '840px',
-                }}
-            >
-                <Box
-                    sx={{
-                        width: { xs: '100%', md: '840px' },
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        alignItems: 'flex-start',
-                        paddingBottom: '30px',
-                        padding: { xs: '20px', md: '0px' },
-                    }}
-                >
-                    <Typography
-                        sx={{
-                            fontWeight: '700',
-                            fontSize: '20px',
-                            color: '#213346',
-                        }}
-                    >
-                        علاقه مندی ها
-                    </Typography>
-                    <Divider sx={{ width: '100%', paddingBottom: '20px' }} />
-                </Box>
-                <Box
-                    sx={{
-                        marginTop: '20px',
-                        borderRadius: '10px',
-                        width: '100%', // Use 100% width for the Box to take full width
-                        maxWidth: '840px', // Set a maximum width for larger screens
-                        // margin: 'auto', // Center the Box
-                        height: 'auto', // Adjust the height as needed
-                        overflowY: 'auto', // Enable vertical scrolling
-                        overflowX: 'hidden',
-                        '&::-webkit-scrollbar': {
-                            display: 'none', // Hide scrollbar for Webkit browsers (Chrome, Safari, etc.)
-                        },
-                        '-ms-overflow-style': 'none', // Hide scrollbar for IE and Edge
-                        'scrollbar-width': 'none', // Hide scrollbar for Firefox
-                    }}
-                >
-                    <Grid
-                        container
-                        spacing={4}
-                        justifyContent="center" // Center Grid items horizontally
-                        sx={{
-                            maxWidth: {md: '840px'},  // Set a maximum width for the Grid container
-                        }}
-                    >
-                        {products.map((product) => (
-                            <Grid item xs={8} sm={6} md={4} key={product.id} sx={{'.muirtl-t94gkw-MuiButtonBase-root-MuiCardActionArea-root':{display: 'flex', alignItems: 'center' }, '.muirtl-kiau9m-MuiGrid-root>.MuiGrid-item': {pl: 0}}}>
-                                <ProductCard product={product} />
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Box>
-            </Box>
-        </Paper>
-    );
+	return (
+		<Paper
+			variant="outlined"
+			sx={{
+				height: "fit-content",
+				borderRadius: "15px",
+				boxShadow: "0px 4px 5px rgba(0, 0, 0, 0.1)",
+			}}>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					flexDirection: "column",
+					padding: { xs: "15px", md: "30px" },
+					maxWidth: "900px",
+					maxHeight: "840px",
+				}}>
+				<Box
+					sx={{
+						width: { xs: "100%", md: "840px" },
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "space-between",
+						alignItems: "flex-start",
+						paddingBottom: "30px",
+						padding: { xs: "20px", md: "0px" },
+					}}>
+					<Typography
+						sx={{
+							fontWeight: "700",
+							fontSize: "20px",
+							color: "#213346",
+						}}>
+						علاقه مندی ها
+					</Typography>
+					<Divider sx={{ width: "100%", paddingBottom: "20px" }} />
+				</Box>
+				<Box
+					sx={{
+						marginTop: "20px",
+						borderRadius: "10px",
+						width: "100%", // Use 100% width for the Box to take full width
+						maxWidth: "840px", // Set a maximum width for larger screens
+						// margin: 'auto', // Center the Box
+						height: "auto", // Adjust the height as needed
+						overflowY: "auto", // Enable vertical scrolling
+						overflowX: "hidden",
+						"&::-webkit-scrollbar": {
+							display: "none", // Hide scrollbar for Webkit browsers (Chrome, Safari, etc.)
+						},
+						"-ms-overflow-style": "none", // Hide scrollbar for IE and Edge
+						"scrollbar-width": "none", // Hide scrollbar for Firefox
+					}}>
+					<Grid
+						container
+						spacing={4}
+						justifyContent="start"
+						sx={{
+							maxWidth: { md: "840px" },
+						}}>
+						{products.map((product) => (
+							<Grid
+								item
+								xs={5.5}
+								sm={5.5}
+								md={4}
+								key={product.id}
+								sx={{
+									".muirtl-t94gkw-MuiButtonBase-root-MuiCardActionArea-root": {
+										display: "flex",
+										alignItems: "center",
+									},
+									".muirtl-kiau9m-MuiGrid-root>.MuiGrid-item": { pl: 0 },
+								}}>
+								<ProductCard product={product} />
+							</Grid>
+						))}
+					</Grid>
+				</Box>
+			</Box>
+		</Paper>
+	);
 }

022fb8e5fdd81c1e34df52f9862f3e8a09b8b488
pourya22334415 <pouryakarami10@gmail.com>
2024-07-19 09:01:28 +0330

    update project with agha parsa

--- src/components/dashboard/DashBoardDubaiOrders.jsx
+++ src/components/dashboard/DashBoardDubaiOrders.jsx
@@ -7,6 +7,7 @@ import { Button, Divider, Grid, Paper, Typography } from "@mui/material";
 import searchIcon from "@/components/siteIcons/SearchIcon.svg";
 import { fetchDubaiOrders } from "@/services/DashBoard";
 import { enqueueSnackbar } from "notistack";
+import ProductNotFound from "../ProductNotFound";
 
 export default function DashBoardDubaiOrders() {
 	const [orders, setOrders] = useState([]);
@@ -100,102 +101,109 @@ export default function DashBoardDubaiOrders() {
 				<Divider sx={{ mt: 2 }} />
 
 				<section className="flex flex-col mt-4 gap-4 overflow-scroll">
-					{filteredOrders.map((product, index) => (
-						<div
-							key={index}
-							className="OrderCard flex gap-6 px-6 py-6 bg-[#F7F7F7] hover:bg-[#0000000e] transition-all rounded-lg lg:flex-col md:gap-4">
-							<div className="rightPart flex w-full flex-col items-end gap-4">
-								<div className="infoRow flex justify-between w-full gap-[10%]">
-									<span className="font-extrabold lg:text-sm whitespace-nowrap">
-										کد پیگیری:{" "}
-									</span>
-									<div className=" lg:text-sm">{product.tracking_code}</div>
-								</div>
 
-								<div className="infoRow flex justify-between w-full gap-[10%]">
-									<span className="font-extrabold lg:text-sm whitespace-nowrap">
-										نام محصول:{" "}
-									</span>
-									<div className=" lg:text-sm">{product.name}</div>
-								</div>
-
-								<div className="infoRow flex justify-between w-full gap-[10%]">
-									<span className="font-extrabold lg:text-sm whitespace-nowrap">
-										سایت درخواست شده:{" "}
-									</span>
-									<div className=" lg:text-sm">{product.website_name}</div>
-								</div>
-
-								<div className="infoRow flex justify-between w-full gap-[10%]">
-									<Typography> لینک محصول: </Typography>
-									<div
-										onClick={() => handleCopyLink(product.link)}
-										title="Click to copy link">
-										<Typography
-											noWrap
-											sx={{ maxWidth: "200px", direction: "rtl" }}>
-											{product.link}
-										</Typography>
+					{filteredOrders.length == 0 ? (
+						<ProductNotFound />
+					) : (
+						<Grid>
+							{filteredOrders.map((product, index) => (	
+								<div
+									key={index}
+									className="OrderCard flex gap-6 px-6 py-6 bg-[#F7F7F7] hover:bg-[#0000000e] transition-all rounded-lg lg:flex-col md:gap-4">
+									<div className="rightPart flex w-full flex-col items-end gap-4">
+										<div className="infoRow flex justify-between w-full gap-[10%]">
+											<span className="font-extrabold lg:text-sm whitespace-nowrap">
+												کد پیگیری:{" "}
+											</span>
+											<div className=" lg:text-sm">{product.tracking_code}</div>
+										</div>
+		
+										<div className="infoRow flex justify-between w-full gap-[10%]">
+											<span className="font-extrabold lg:text-sm whitespace-nowrap">
+												نام محصول:{" "}
+											</span>
+											<div className=" lg:text-sm">{product.name}</div>
+										</div>
+		
+										<div className="infoRow flex justify-between w-full gap-[10%]">
+											<span className="font-extrabold lg:text-sm whitespace-nowrap">
+												سایت درخواست شده:{" "}
+											</span>
+											<div className=" lg:text-sm">{product.website_name}</div>
+										</div>
+		
+										<div className="infoRow flex justify-between w-full gap-[10%]">
+											<Typography> لینک محصول: </Typography>
+											<div
+												onClick={() => handleCopyLink(product.link)}
+												title="Click to copy link">
+												<Typography
+													noWrap
+													sx={{ maxWidth: "200px", direction: "rtl" }}>
+													{product.link}
+												</Typography>
+											</div>
+										</div>
+									</div>
+		
+									<div className="leftPart w-full flex flex-col gap-4">
+										<div className="infoRow flex justify-between w-full gap-[10%]">
+											<span className="font-extrabold md:text-sm whitespace-nowrap">
+												تاریخ استعلام:{" "}
+											</span>
+											<span className=" md:text-sm">{product.shamsi_date}</span>
+										</div>
+		
+										<div className="infoRow flex justify-between w-full gap-[10%]">
+											<span className="font-extrabold md:text-sm whitespace-nowrap">
+												قیمت تومانی:{" "}
+											</span>
+											<div className="flex gap-1">
+												<span className=" md:text-sm">{product.toman_price}</span>
+												<span className=" md:text-sm">تومان</span>
+											</div>
+										</div>
+		
+										<div className="infoRow flex justify-between w-full gap-[10%]">
+											<span className="font-extrabold lg:text-sm text-palette-blue whitespace-nowrap">
+												قیمت نهایی با ارسال:{" "}
+											</span>
+											<div className="flex gap-1 font-extrabold text-palette-blue">
+												<span className=" lg:text-sm">{product.toman_total}</span>
+												<span className=" lg:text-sm">تومان</span>
+											</div>
+										</div>
+		
+										<div className="infoRow flex w-full justify-center md:justify-center">
+											<Button
+												className="w-[43%]"
+												variant="contained"
+												sx={{
+													bgcolor: product.admin_checked ? Colors.orange : "grey", // Optional: Change color when disabled
+													color: "black",
+													borderRadius: "50px",
+													boxShadow: "none",
+													mt: 1,
+													height: 34,
+													"&:hover": {
+														bgcolor: product.admin_checked ? Colors.orange : "grey", // Keep bgcolor the same on hover when disabled
+													},
+												}}
+												disabled={!product.admin_checked} // Button is disabled if product.admin_checked is false
+											>
+												<Typography
+													variant="div"
+													component="div"
+													sx={{ fontSize: "0.875rem", whiteSpace: "nowrap" }}>
+													ثبت سفارش
+												</Typography>
+											</Button>
+										</div>
 									</div>
 								</div>
-							</div>
-
-							<div className="leftPart w-full flex flex-col gap-4">
-								<div className="infoRow flex justify-between w-full gap-[10%]">
-									<span className="font-extrabold md:text-sm whitespace-nowrap">
-										تاریخ استعلام:{" "}
-									</span>
-									<span className=" md:text-sm">{product.shamsi_date}</span>
-								</div>
-
-								<div className="infoRow flex justify-between w-full gap-[10%]">
-									<span className="font-extrabold md:text-sm whitespace-nowrap">
-										قیمت تومانی:{" "}
-									</span>
-									<div className="flex gap-1">
-										<span className=" md:text-sm">{product.toman_price}</span>
-										<span className=" md:text-sm">تومان</span>
-									</div>
-								</div>
-
-								<div className="infoRow flex justify-between w-full gap-[10%]">
-									<span className="font-extrabold lg:text-sm text-palette-blue whitespace-nowrap">
-										قیمت نهایی با ارسال:{" "}
-									</span>
-									<div className="flex gap-1 font-extrabold text-palette-blue">
-										<span className=" lg:text-sm">{product.toman_total}</span>
-										<span className=" lg:text-sm">تومان</span>
-									</div>
-								</div>
-
-								<div className="infoRow flex w-full justify-center md:justify-center">
-									<Button
-										className="w-[43%]"
-										variant="contained"
-										sx={{
-											bgcolor: product.admin_checked ? Colors.orange : "grey", // Optional: Change color when disabled
-											color: "black",
-											borderRadius: "50px",
-											boxShadow: "none",
-											mt: 1,
-											height: 34,
-											"&:hover": {
-												bgcolor: product.admin_checked ? Colors.orange : "grey", // Keep bgcolor the same on hover when disabled
-											},
-										}}
-										disabled={!product.admin_checked} // Button is disabled if product.admin_checked is false
-									>
-										<Typography
-											variant="div"
-											component="div"
-											sx={{ fontSize: "0.875rem", whiteSpace: "nowrap" }}>
-											ثبت سفارش
-										</Typography>
-									</Button>
-								</div>
-							</div>
-						</div>
-					))}
+							))}
+						</Grid>
+					)}
 				</section>
 			</section>
 		</Paper>

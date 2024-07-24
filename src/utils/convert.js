dc66f6247ead96049ae1d50c7211296601d4c33c
reza buzarjemehri <dev.reza21@gmail.com>
2024-07-24 21:28:40 +0330

    use Grouping in convert

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -1,32 +1,32 @@
 const convert = (number) => {
 	return number.toLocaleString("fa-IR", {
-		useGrouping: false,
+		useGrouping: true,
 	});
 };
 
 export default convert;
 
 export const toPersianDigits = (number) => {
-    if (number === undefined || number === null) {
-        // console.error('Invalid input: number is undefined or null');
-        return number;
-    }
-    const persianNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
-    return number.toString().replace(/\d/g, (x) => persianNumbers[x]);
+	if (number === undefined || number === null) {
+		// console.error('Invalid input: number is undefined or null');
+		return number;
+	}
+	const persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+	return number.toString().replace(/\d/g, (x) => persianNumbers[x]);
 };
 
 export const getGenderDescription = (genderCode) => {
-    if (!genderCode) {
-        return 'خالی';
-    }
-    const genderMap = {
-        'm': 'آقا',
-        'f': 'خانم',
-        'u': 'مایل به گفتن ندارم'
-    };
-    return genderMap[genderCode.toLowerCase()] || 'خالی';
+	if (!genderCode) {
+		return "خالی";
+	}
+	const genderMap = {
+		m: "آقا",
+		f: "خانم",
+		u: "مایل به گفتن ندارم",
+	};
+	return genderMap[genderCode.toLowerCase()] || "خالی";
 };
 
 export const createFullName = (firstName, lastName) => {
-    return `${firstName} ${lastName}`;
+	return `${firstName} ${lastName}`;
 };

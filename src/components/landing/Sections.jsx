424c9d9e93602239b9b98e1da5b348aef20c1407
reza buzarjemehri <dev.reza21@gmail.com>
2024-07-24 21:47:15 +0330

    Update Sections.jsx

--- src/components/landing/Sections.jsx
+++ src/components/landing/Sections.jsx
@@ -23,8 +23,8 @@ function Sections({ data }) {
 							}}>
 							<CardMedia
 								sx={{
-									width: { xs: 156, md: 260 },
-									height: { xs: 168, md: 280 },
+									width: { xs: 130, md: 260 },
+									height: { xs: 140, md: 280 },
 									position: "absolute",
 									mb: 5,
 									px: "auto",
@@ -36,23 +36,26 @@ function Sections({ data }) {
 								<Box
 									sx={{
 										px: "auto",
-										height: { xs: 84, md: 140 },
+										height: { xs: 70, md: 140 },
 									}}
 								/>
 								<Box
 									sx={{
 										border: "3px Dashed #22668D",
 										px: "auto",
-										width: { xs: 156, md: 260 },
-										height: { xs: 84, md: 140 },
+										width: { xs: 130, md: 260 },
+										height: { xs: 70, md: 140 },
 										bgcolor: `#${item.color}`,
 										borderRadius: "100px",
 									}}
 								/>
 							</Box>
 							<Typography
-								pt={2}
 								variant="h6"
+								sx={{
+									pt: { xs: 1, md: 2 },
+									fontSize: { xs: 16, md: 24 },
+								}}
 								fontWeight="800"
 								display="block"
 								noWrap={true}
